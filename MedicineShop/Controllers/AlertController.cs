b8a9d64131af59cd8cb1480241f139e57277dc97
Mursalin Leon <mursalinleon92@gmail.com>
2026-05-19 10:31:34 +0600

    complete:

--- MedicineShop/Controllers/AlertController.cs
+++ MedicineShop/Controllers/AlertController.cs
@@ -1,4 +1,6 @@
-﻿using BLL.Services;
+﻿using System;
+using System.Linq;
+using BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedicineShop.Controllers
@@ -12,18 +14,29 @@ namespace MedicineShop.Controllers
             _reportService = reportService;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
             if (!(IsAdmin || IsStaff))
             {
                 return LoginRedirect();
             }
 
+            const int pageSize = 15;
+            var lowStockMedicines = _reportService.GetLowStockMedicines();
+            var totalCount = lowStockMedicines.Count;
+            var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            page = Math.Max(1, Math.Min(page, totalPages == 0 ? 1 : totalPages));
+            var pagedLowStockMedicines = lowStockMedicines.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
             var reportDto = new BLL.DTOs.ReportDTO
             {
                 DashboardSummary = _reportService.GetDashboardSummary(),
-                LowStockMedicines = _reportService.GetLowStockMedicines()
+                LowStockMedicines = pagedLowStockMedicines
             };
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.PageSize = pageSize;
 
             return View(reportDto);
         }
--- MedicineShop/Controllers/ReportController.cs
+++ MedicineShop/Controllers/ReportController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using BLL.DTOs;
 using BLL.Services;
 
@@ -13,18 +15,29 @@ namespace MedicineShop.Controllers
             _reportService = reportService;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
             if (!IsAdmin)
             {
                 return LoginRedirect();
             }
 
+            const int pageSize = 15;
+            var lowStockMedicines = _reportService.GetLowStockMedicines();
+            var totalCount = lowStockMedicines.Count;
+            var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            page = Math.Max(1, Math.Min(page, totalPages == 0 ? 1 : totalPages));
+            var pagedLowStockMedicines = lowStockMedicines.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
             var reportDto = new ReportDTO
             {
                 DashboardSummary = _reportService.GetDashboardSummary(),
-                LowStockMedicines = _reportService.GetLowStockMedicines()
+                LowStockMedicines = pagedLowStockMedicines
             };
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.PageSize = pageSize;
 
             return View(reportDto);
         }
